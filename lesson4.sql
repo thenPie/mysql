871c568d79a1a50f6c14a6b3d11a2a2af0b719c3
thenPie <118397975+thenPie@users.noreply.github.com>
2023-07-11 19:13:50 +0300

    Update lesson4.sql

--- lesson4.sql
+++ lesson4.sql
@@ -2,4 +2,10 @@ SELECT shops.shopname, cats.name
 FROM cats
 INNER JOIN  shops ON cats.shops_id=shops.id;
 
-
+SELECT shopname
+	FROM (
+	    SELECT shops.shopname, cats.name
+	    FROM cats
+	    INNER JOIN  shops ON cats.shops_id=shops.id
+	) AS T
+WHERE name = 'Murzik';
