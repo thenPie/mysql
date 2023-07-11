6e8fe8ba74f5699639f852d92b150096a9838779
thenPie <118397975+thenPie@users.noreply.github.com>
2023-07-11 19:29:28 +0300

    Update lesson4.sql

--- lesson4.sql
+++ lesson4.sql
@@ -16,10 +16,8 @@ JOIN cats
 ON shops.id=cats.shops_id
 WHERE cats.name = 'Murzik';
 
-SELECT shopname
-	FROM (
-	    SELECT shops.shopname, cats.name
-	    FROM cats
-	    INNER JOIN  shops ON cats.shops_id=shops.id
-	) AS T
-WHERE name != 'Murzik' AND name != 'Zuza';
+SELECT shops.shopname, cats.name
+FROM shops
+JOIN cats
+ON shops.id=cats.shops_id
+WHERE cats.name NOT IN ('Murzik', 'Zuza');
