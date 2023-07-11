c6408ec69ce72cd7c823e2030aa950b6c51411d5
thenPie <118397975+thenPie@users.noreply.github.com>
2023-07-11 19:36:11 +0300

    Update lesson4.sql

--- lesson4.sql
+++ lesson4.sql
@@ -21,3 +21,8 @@ FROM shops
 JOIN cats
 ON shops.id=cats.shops_id
 WHERE cats.name NOT IN ('Murzik', 'Zuza');
+
+SELECT * FROM analysis
+JOIN orders ON analysis.an_id=orders.ord_an
+WHERE DATE (orders.ord_datetime) BETWEEN '2020-02-05' AND '2020-02-05' + INTERVAL 1 WEEK
+ORDER by orders.ord_datetime;
