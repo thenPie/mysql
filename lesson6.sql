dfed8bf852600c540c5a42e23d09fd49b5e9a219
thenPie <118397975+thenPie@users.noreply.github.com>
2023-07-15 18:22:04 +0300

    Update lesson6.sql

--- lesson6.sql
+++ lesson6.sql
@@ -3,11 +3,11 @@ SELECT dater(seconds) AS 'DD:HH:MM' FROM date_it;
 CREATE DEFINER=`root`@`localhost` FUNCTION `dater`(seconds INT) RETURNS char(50) CHARSET utf8mb4
     DETERMINISTIC
 BEGIN
-	DECLARE days char(50);
+  DECLARE days char(50);
   DECLARE hm char(50);
   DECLARE res char(50);
   SET days = FLOOR(HOUR(SEC_TO_TIME(seconds)) / 24);
   SET hm = TIME_FORMAT(SEC_TO_TIME(seconds % (24 * 60 * 60)), '%H:%i');
   SET res = CONCAT(days, ':', hm);
-	RETURN (res);
+  RETURN (res);
 END
